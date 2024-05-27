0dc05540e44d6e604b178f0d5ec879ef68cc7044
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-05-28 00:43:41 +0530

    Time: 3 ms (73.62%) | Memory: 42.6 MB (91.85%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start < end){
+            Character low = s.charAt(start);
+            Character high = s.charAt(end);
+            if(!Character.isLetterOrDigit(low)){
+                start++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(high)){
+                end--;
+                continue;
+            }
+            if(Character.toLowerCase(low) != Character.toLowerCase(high))
+                return false;
+            else{
+                start++; end--;
+            }
+        }
+        return true;
+    }
+}
