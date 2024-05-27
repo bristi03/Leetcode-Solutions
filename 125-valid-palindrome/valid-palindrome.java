9665a16d63d985444ec2b318b27a790bf63035ff
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-05-28 00:45:58 +0530

    Time: 3 ms (73.62%) | Memory: 42.5 MB (91.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
+        if(s.isEmpty()) {
+        	return true;
+        }
         int start = 0;
         int end = s.length() - 1;
 
