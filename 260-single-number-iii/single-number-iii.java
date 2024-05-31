1fd671059cda96e57e4993dddb1a41c8f51d9389
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-05-31 13:29:07 +0530

    Time: 3 ms (45.25%) | Memory: 45.6 MB (53.10%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -4,12 +4,18 @@ class Solution {
         int[] result = new int[2];
         int index = 0;
         for(int i=0; i< nums.length; i++){
-            if(i==0 && nums[i] != nums[i+1])
-                result[index++] = nums[i];
-            else if(i == nums.length-1 && nums[i] != nums[i-1])
-                result[index++] = nums[i];
-            else if(i>0 && nums[i] != nums[i+1] && nums[i] != nums[i-1])
-                result[index++] = nums[i];
+            if(i==0){ 
+                if (nums[i] != nums[i+1]) 
+                    result[index++] = nums[i];
+                }
+            else if(i == nums.length-1){ 
+                if(nums[i] != nums[i-1]) 
+                    result[index++] = nums[i];
+                }
+            else if(i>0 && i < nums.length-1){
+                if(nums[i] != nums[i+1] && nums[i] != nums[i-1])
+                    result[index++] = nums[i];
+                }
             if(index == 2)
                 break;
         }
