bb83d95d2f60baa0258db1949ddfe2deabc6099f
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-05-31 14:16:04 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (90.35%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
+        //XOR all numbers to find the XOR of the two unique numbers
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        
+        //Find a set bit in the xor result
+        int setBit = xor & -xor;  // This isolates the rightmost set bit
+        
+        //Divide numbers into two groups and XOR each group
         int[] result = new int[2];
-        int index = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num:nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+        for (int num : nums) {
+            if ((num & setBit) == 0) {
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
         }
-
-        for(Map.Entry<Integer, Integer> it:map.entrySet()){
-            if(it.getValue()==1)
-                result[index++] = it.getKey();
-            if(index == 2)
-                break;
-        }
+        
         return result;
     }
 }
