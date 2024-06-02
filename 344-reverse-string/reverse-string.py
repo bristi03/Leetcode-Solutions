43c6a160a57b89838f720033d861a7840a8b5685
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-06-02 13:16:30 +0530

    Time: 135 ms (39.84%) | Memory: 19.2 MB (80.03%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,11 +4,4 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        start = 0
-        end = len(s)-1
-        while(start < end):
-            temp = s[start]
-            s[start] = s[end]
-            s[end] = temp
-            start += 1
-            end -= 1
+        s.reverse()
