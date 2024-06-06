fa4d2b287ecf415731b21b177f584e8be9940bd9
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-06-06 16:45:59 +0530

    Time: 9 ms (99.76%) | Memory: 44.5 MB (99.46%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -5,21 +5,26 @@ class Solution {
         Arrays.sort(hand);
         for(int i=0; i<hand.length; i++){
             if(hand[i] == -1) continue;
-            int current = hand[i];
-            int search = current + 1;
-            int count = 1;
-            for(int j=i+1; j<hand.length; j++){
-                if( hand[j] == search){
-                    search += 1;
-                    count += 1;
-                    hand[j] = -1;
-                }
-                if(count == groupSize)
-                    break;
-            }
-            if(count != groupSize)
+            int curridx = i;
+            if(!hasGroup(curridx, hand, groupSize))
                 return false;
         }
         return true;
+    }
+
+    boolean hasGroup(int i, int[] hand, int groupSize){
+        int current = hand[i];
+        int search = current + 1;
+        int count = 1;
+        for(int j=i+1; j<hand.length; j++){
+            if( hand[j] == search){
+                search += 1;
+                count += 1;
+                hand[j] = -1;
+            }
+            if(count == groupSize)
+                return true;
+        }
+        return false;
     }
 }
