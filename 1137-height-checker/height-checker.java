293e496060402d824b1e10ed268b923e141d9e0b
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-06-11 00:28:44 +0530

    Time: 1 ms (92.84%) | Memory: 41.5 MB (19.46%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,15 +1,17 @@
 class Solution {
     public int heightChecker(int[] heights) {
+        //count sort
         int m=0;
         for(int h: heights){
             m = Math.max(m,h);
         }
         int[] expected = new int[m+1];
-
+        // Count the occurrences of each height
         for(int h:heights){
             expected[h]++;
         }
         int j=1, i=0, count=0;
+        // Compare the original array with the sorted array constructed from the frequency array
         while(i<heights.length){
             while(j<m+1 && expected[j]>0){
                 if(heights[i++] != j)
