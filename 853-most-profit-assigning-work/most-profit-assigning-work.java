b662022136f85c49e862d813d7fabd77a49b5c94
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-06-18 14:01:45 +0530

    Time: 18 ms (79.48%) | Memory: 45.9 MB (31.08%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,21 +1,28 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        // Combine profit and difficulty into a list of jobs
         List<int[]> jobs = new ArrayList<>();
         for(int i=0; i<profit.length; i++){
             jobs.add(new int[]{profit[i], difficulty[i]});
         }
+        // Sort jobs by profit in ascending order
         Collections.sort(jobs, (a, b)->Integer.compare(a[0], b[0]));
+        // Sort workers by their ability in ascending order
         Arrays.sort(worker);
-        int j= jobs.size()-1;
-        int bestProfit =0, total = 0;
+
+        int j= jobs.size()-1; // Start from the most profitable job
+        int total = 0;
+        // Traverse the workers from the most capable to the least
         for(int i= worker.length-1; i>=0; i--){
-            int ability = worker[i];
+            int ability = worker[i]; // Current worker's ability
+
+            // Find the most profitable job the worker can perform
             while(j >= 0 ){
-                if(jobs.get(j)[1] <= ability){
+                if(jobs.get(j)[1] <= ability){ // Job difficulty is less than or equal to worker's ability
                     total += jobs.get(j)[0];
-                    break;
+                    break; // Move to the next worker
                 }
-                j--;
+                j--; // Move to the next less profitable job
             }
         }
         return total;
