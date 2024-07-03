20fee5add1558b7540d74994bd596ccf71765363
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-07-03 17:57:34 +0530

    Time: 15 ms (96.06%) | Memory: 57.2 MB (29.92%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,15 +1,21 @@
 class Solution {
     public int minDifference(int[] nums) {
-        if(nums.length <= 4) // k+1 = 4
+        // If the array has 4 or fewer elements, we can make all elements equal with at most 3 changes.
+        if(nums.length <= 4)
             return 0;
-        int k = 3;
-        Arrays.sort(nums);
-        int windowSize = nums.length - k;
-        int ans = nums[windowSize-1] - nums[0];
-        for(int i=1; i<= nums.length- windowSize; i++){
-            int diff = nums[windowSize - 1 + i] - nums[i];
-            ans = Math.min(ans,diff);
+        int k = 3; // We can make at most 3 changes, taken for generalization
+        Arrays.sort(nums); 
+        int windowSize = nums.length - k; // Size of the window of elements we will consider
+
+        // Initialize the minimum difference with the difference of the first window
+        int ans = nums[windowSize - 1] - nums[0];
+
+        // Slide the window across the sorted array to find the minimum difference
+        for(int i = 1; i <= nums.length - windowSize; i++) {
+            int diff = nums[windowSize - 1 + i] - nums[i]; // Difference of current window
+            ans = Math.min(ans, diff); // Update the minimum difference if the current one is smaller
         }
-        return ans;
+
+        return ans; 
     }
 }
