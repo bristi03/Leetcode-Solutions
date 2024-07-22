eb70f546321081867457e34d07eef4ee39161c01
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-07-22 17:07:28 +0530

    Time: 2 ms (96.87%) | Memory: 45.4 MB (18.45%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,29 +1,34 @@
 class Solution {
-    static class Person{
-        String name;
-        int height;
-
-        Person(String name, int height){
-            this.name = name;
-            this.height = height;
-        }
-    }
     public String[] sortPeople(String[] names, int[] heights) {
-        int n= names.length;
-        List<Person> people = new ArrayList<>();
-
-        for(int i=0; i< n; i++){
-            people.add(new Person(names[i], heights[i]));
-        }
-
-        //sort people in desc order of heights
-        Collections.sort(people, (a,b)-> b.height - a.height);
-
-        int i=0;
-        for(Person person: people){
-            names[i++] = person.name;
-        }
-
+        quickSort(names, heights, 0, names.length-1);
         return names;
     }
+    void quickSort(String[] names, int[] heights, int left, int right){
+        if(left >= right)
+            return;
+
+        int last = left;
+        int pivot = (left + right)/2;
+
+        swap(names, heights, left, pivot);
+        for(int i= left+1; i<= right; i++){
+            if( heights[i] > heights[left]){ //sorting in desc order of heights
+                swap(names, heights, ++last, i);
+            }
+        }
+        swap(names, heights, last, left);
+        quickSort(names, heights, left, last-1);
+        quickSort(names, heights, last+1, right);
+    }
+
+    void swap(String[] names, int[] heights, int i, int j){
+        int temp = heights[i];
+        heights[i] = heights[j];
+        heights[j] = temp;
+
+        String tempName = names[i];
+        names[i] = names[j];
+        names[j] = tempName;
+    }
+
 }
