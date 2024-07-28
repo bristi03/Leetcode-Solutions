5a018abfc410ab255e612b827c390df854217ba4
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-07-28 20:15:14 +0530

    Time: 13 ms (94.01%) | Memory: 45.6 MB (72.81%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,32 +1,42 @@
 class Solution {
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        int[][] dis = new int[26][26];
-        for (int i = 0; i < 26; i++) {
-            Arrays.fill(dis[i], Integer.MAX_VALUE);
-            dis[i][i] = 0;
+        int[][] dist = new int[26][26];
+        for(int[] row:dist){
+            Arrays.fill(row, Integer.MAX_VALUE);
         }
-        for (int i = 0; i < cost.length; i++) {
-            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'], cost[i]);
+        for(int i=0; i<26; i++){
+            dist[i][i] = 0;
         }
-        for (int k = 0; k < 26; k++) {
-            for (int i = 0; i < 26; i++)
-                if (dis[i][k] < Integer.MAX_VALUE) {
-                    for (int j = 0; j < 26; j++) {
-                        if (dis[k][j] < Integer.MAX_VALUE) {
-                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+        for(int i=0; i<original.length; i++){
+            int from=  original[i] - 'a';
+            int to = changed[i] - 'a';
+
+            dist[from][to] = Math.min(dist[from][to], cost[i]);
+        }
+
+        for(int k=0; k<26; k++){
+            for(int i=0; i<26; i++){
+                if(dist[i][k] < Integer.MAX_VALUE){
+                    for(int j=0; j<26; j++){
+                        if(dist[k][j] < Integer.MAX_VALUE){
+                            if(dist[i][j] > dist[i][k] + dist[k][j]){
+                                dist[i][j] = dist[i][k] + dist[k][j];
+                            }
                         }
                     }
                 }
-        }
-        long ans = 0L;
-        for (int i = 0; i < source.length(); i++) {
-            int c1 = source.charAt(i) - 'a';
-            int c2 = target.charAt(i) - 'a';
-            if (dis[c1][c2] == Integer.MAX_VALUE) {
-                return -1L;
-            } else {
-                ans += (long)dis[c1][c2];
             }
+        }
+
+        long ans = 0;
+        for(int i=0; i<source.length(); i++){
+            int from = source.charAt(i)-'a';
+            int to = target.charAt(i) - 'a';
+            if(dist[from][to] == Integer.MAX_VALUE){
+                return -1;
+            }
+            else
+                ans += (long)dist[from][to];
         }
         return ans;
     }
