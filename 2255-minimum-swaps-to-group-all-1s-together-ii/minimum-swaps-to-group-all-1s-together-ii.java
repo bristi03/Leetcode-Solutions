33b43287c0ce5f3a47d81047172d39a012a48b73
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-08-03 02:02:45 +0530

    Time: 13 ms (13.71%) | Memory: 59.2 MB (58.38%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,32 +1,24 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int n = nums.length;
+        int n= nums.length;
         int totalCount = 0;
-
-        // Count the total number of 1s in the array
-        for (int num : nums) {
-            if (num == 1) {
+        int currCons = 0;
+        int maxCons = 0;
+        for(int num:nums){
+            if(num == 1)
                 totalCount++;
-            }
         }
-
-        // If there are no 1s, no swaps are needed
-        if (totalCount == 0) return 0;
-
-        // Find the maximum number of 1s in any window of size totalCount
-        int maxCons = 0, currentCount = 0;
-        for (int i = 0; i < 2 * n; i++) {
-            if (nums[i % n] == 1) {
-                currentCount++;
+        for(int i=0; i< 2*n; i++){
+            if(nums[i%n] == 1){
+                currCons++;
             }
-            if (i >= totalCount) {
-                if (nums[(i - totalCount) % n] == 1) {
-                    currentCount--;
+            if(i >= totalCount){
+                if(nums[(i-totalCount) % n]==1){
+                    currCons--;
                 }
+                maxCons = Math.max(maxCons, currCons);
             }
-            maxCons = Math.max(maxCons, currentCount);
         }
-
         return totalCount - maxCons;
     }
 }
