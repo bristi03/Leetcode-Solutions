17ef333a0ea2de8b2d0062763588e07c07de7d86
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-08-03 16:29:08 +0530

    Time: 7 ms (28.08%) | Memory: 44.1 MB (56.26%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,7 +1,21 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Arrays.sort(target);
-        Arrays.sort(arr);
-        return Arrays.equals(target, arr);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num:target){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num:arr){
+            if(map.containsKey(num)){
+                if(map.get(num) > 1){
+                    map.put(num,map.get(num)-1);
+                }
+                else{
+                    map.remove(num);
+                }
+            }
+            else
+                return false;
+        }
+        return map.isEmpty();
     }
 }
