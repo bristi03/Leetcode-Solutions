0a1f848b00f91b40b91ecd6340b39fdb9faf9aa0
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-08-06 22:24:01 +0530

    Time: 9 ms (61.15%) | Memory: 45.6 MB (64.04%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,22 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         for(char c: word.toCharArray()){
-            map.put(c, map.getOrDefault(c,0)+1);
-        }
-        int n = map.size();
-        if(n <= 8){
-            return word.length();
-        }
-        int[] freq = new int[n];
-        int index = 0;
-        for(Map.Entry<Character, Integer> entry: map.entrySet()){
-            freq[index++] = entry.getValue();
+            freq[c - 'a']++;
         }
         Arrays.sort(freq);
         int count = 0, press = 1;
         int minPress = 0;
-        for(int i=n-1; i>=0; i--){
+        for(int i=25; i>=0; i--){
+            if(freq[i] == 0) break; 
+            
             minPress += freq[i] * press;
             count++;
 
