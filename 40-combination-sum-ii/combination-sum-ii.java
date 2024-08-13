5b9da4d16f7711f6da143ccd25e6c8d6702cd89f
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-08-13 15:00:11 +0530

    Time: 4 ms (46.33%) | Memory: 43.1 MB (71.22%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates); // Sort candidates to handle duplicates
+        List<Integer> ds = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        subsetRec(candidates, target, 0, ds, res);
+        return res;
+    }
+    void subsetRec(int[] arr,int target, int index, List<Integer> ds,List<List<Integer>> res ){
+        if (target == 0) {
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        if (target < 0 || index == arr.length) {
+             return;
+        }
+        ds.add(arr[index]);
+        subsetRec(arr, target-arr[index], index + 1, ds, res);
+        ds.remove(ds.size() - 1);
+
+        while(index < arr.length-1 && arr[index] == arr[index+1])
+        index++;
+        subsetRec(arr, target, index + 1, ds, res);
+    }
+}
