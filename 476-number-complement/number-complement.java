ae81b79197c89de07647d883ec90b49575516fd1
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-08-22 17:39:10 +0530

    Time: 13 ms (5.05%) | Memory: 41.6 MB (6.96%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,10 +1,14 @@
 class Solution {
     public int findComplement(int num) {
-        String s = Integer.toBinaryString(num);
-        s = s.replaceAll("0", "2");
-        s = s.replaceAll("1", "0");
-        s = s.replaceAll("2", "1");
-        int toggled = Integer.parseInt(s, 2);
+        String bin = "";
+        while(num > 0){
+            bin = (num&1) + bin;
+            num= num>>1;
+        }
+        bin = bin.replaceAll("0", "2");
+        bin = bin.replaceAll("1", "0");
+        bin = bin.replaceAll("2", "1");
+        int toggled = Integer.parseInt(bin, 2);
         return toggled;
     }
 }
